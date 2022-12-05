9c583a901e695c2e1286206862cea9afeb927411
Vivian Zitek <vivian.zitek@gmail.com>
2022-12-05 09:37:28 -0700

    Add basic menu outline.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿// CS1400 - Extra Credit, Tic-Tac-Toe
+﻿using menudriver;
+
+// CS1400 - Extra Credit, Tic-Tac-Toe
 // Pluto Zitek, 2022-12-05
 
 string pieceCross = @"                
@@ -17,5 +19,33 @@ string pieceCircle = @"
     ▝██████▘    
                 ";
 
-Console.WriteLine(pieceCross);
-Console.WriteLine(pieceCircle);
+
+MenuDriver selectionMenu = new MenuDriver();
+string[] options = new string[] {
+
+	"New Game",
+	"Exit Program",
+};
+selectionMenu.AddOptions(options);
+
+void mainMenu() {
+	while (selectionMenu.menuLoop){
+
+		Console.Clear();
+		selectionMenu.DisplayMenu();
+		selectionMenu.SetMenuCursor();
+
+		switch(selectionMenu.selectedItem) {
+
+			case 0:
+				continue;
+			case 1:
+				Environment.Exit(0);
+				break;
+			default:
+				break;
+		}
+	}
+}
+
+mainMenu();
